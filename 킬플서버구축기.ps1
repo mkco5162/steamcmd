c1f9bc559d008969180153f4f7e4da58c2206ff2
mkco5162 <49329821+mkco5162@users.noreply.github.com>
2021-04-30 12:42:23 +0900

    Update 킬플서버구축기.ps1

--- 킬플서버구축기.ps1
+++ 킬플서버구축기.ps1
@@ -1,5 +1,5 @@
 ﻿echo "킬링플로어2 데디케이트 서버 구축기  by. ㅇㅇ(1.239)"
-echo "version : 0.2"
+echo "version : 0.2.1"
 $install = Read-Host "킬링플로어2 데디케이트 서버를 설치할 폴더를 지정해주세요"
 $steamcmd = $install + "\cmd"
 mkdir $steamcmd
@@ -18,11 +18,13 @@ Start-Sleep -s 3
 wait-process -name steamcmd
 $Serverinstall = $install + "\Binaries\win64\KFServer"
 Start-Process $Serverinstall
-Start-Sleep -s 5
 echo ""
-echo "서버 정상 실행이 완료되면 종료해주시기 바랍니다"
+echo "최초 서버 실행이 완료되면 20초 후 설치가 계속됩니다"
+#echo "서버 정상 실행이 완료되면 종료해주시기 바랍니다"
 echo ""
-wait-process -name kfserver
+Start-Sleep -s 23
+Stop-Process -Name "kfserver"
+#wait-process -name kfserver
 
 $Filepath1 = $install + "\KFGame\Config\PCServer-KFEngine.ini"
 (Get-Content $Filepath1).replace("[IpDrv.TcpNetDriver]","[IpDrv.TcpNetDriver]`nDownloadManagers=OnlineSubsystemSteamworks.SteamWorkshopDownload") | Set-Content $Filepath1
@@ -42,7 +44,7 @@ echo "지금부터 게임 세부설정을 진행합니다."
 echo "기본값으로 사용하시려면 그냥 엔터를 눌러주시기 바랍니다."
 echo ""
 echo "게임 포트 : 서버 접속시 이용하는 포트 (기본값 : 7777)"
-$server_gameport = Read-Host "게임 포트 입력 : "
+$server_gameport = Read-Host "게임 포트 입력 "
 echo ""
 if (0 -eq $server_gameport)
 {
@@ -51,7 +53,7 @@ if (0 -eq $server_gameport)
 }
 echo ""
 echo "쿼리 포트 : 스팀과 통신하는데 사용하는 포트 (기본값 : 27015)"
-$server_queryport = Read-Host "쿼리 포트 입력 : "
+$server_queryport = Read-Host "쿼리 포트 입력 "
 echo ""
 if (0 -eq $server_queryport)
 {
@@ -60,7 +62,7 @@ if (0 -eq $server_queryport)
 }
 echo ""
 echo "웹어드민 포트 : 웹어드민 접속시 이용하는 포트 (기본값 : 8080)"
-$server_webadminport = Read-Host "웹어드민 포트 입력 : "
+$server_webadminport = Read-Host "웹어드민 포트 입력 "
 echo ""
 if (0 -eq $server_webadminport)
 {
@@ -82,9 +84,9 @@ echo "스팀 포트        20560        UDP"
 echo "NTP 포트         123          UDP"
 echo ""
 echo ""
-$server_webadminpassword = Read-Host "웹어드민에 사용할 암호 입력 : "
+$server_webadminpassword = Read-Host "웹어드민에 사용할 암호 입력 "
 echo ""
-$server_difficulty = Read-Host "서버 난이도 설정 (보통:0, 어려움:1, 자살행위:2, 생지옥:3): "
+$server_difficulty = Read-Host "서버 난이도 설정 (보통:0, 어려움:1, 자살행위:2, 생지옥:3) "
 echo ""
 echo "서버 실행은 서버 설치 폴더에 가면 서버실행기.bat이 있습니다 해당 파일을 더블클릭하여 실행합니다."
 Read-Host -Prompt "설정이 완료되었습니다. 엔터를 눌러 설치를 종료합니다"
